4967fb4261640b5d7314f84f5bacb5b8cbfaba85
Franca28 <felipe.oliveira.dev2025@gmail.com>
2025-11-11 22:12:54 -0300

    finalllllllllllllllllll

--- JOGO/js/banheiroScript.js
+++ JOGO/js/banheiroScript.js
@@ -4,7 +4,7 @@ function quebrarEspelho() {
     if (tentativa == 3) {
         alert("Você quebra o espelho e acha uma chave escondida")
         alert("Você destranca a porta da entrada e saí vivo!")
-        window.location.href = "/index"
+        window.location.href = "/index.html"
     } else {
         alert("Você não consegue quebrar, mas não desista!")
         tentativa++;
